a34677ee9f41ec52ed3a503ac6a75ee6d59dd6ff
yishen <yishen@localhost>
2023-03-31 20:00:18 -0600

    update scripts

--- install_grpc.sh
+++ install_grpc.sh
@@ -15,12 +15,11 @@ sudo apt install -y build-essential autoconf libtool pkg-config libsystemd-dev
 git clone --recurse-submodules -b v1.52.0 --depth 1 --shallow-submodules https://github.com/grpc/grpc
 
 cd grpc
-mkdir -p cmake/build
-pushd cmake/build
+mkdir -p cmake/build && cd cmake/build
 cmake -DgRPC_INSTALL=ON \
       -DgRPC_BUILD_TESTS=OFF \
       -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
       ../..
 make -j 4
 make install
-popd
+cd ../..
--- install_redis.sh
+++ install_redis.sh
@@ -1,5 +1,6 @@
 sudo apt install redis-server
-
+export MY_INSTALL_DIR=$HOME/.local
+export PATH="$MY_INSTALL_DIR/bin:$PATH"
 # Clone the project
 git clone https://github.com/Cylix/cpp_redis.git
 # Go inside the project directory
@@ -7,10 +8,12 @@ cd cpp_redis
 # Get tacopie submodule
 git submodule init && git submodule update
 # Create a build directory and move into it
-mkdir build && cd build
+mkdir -p build && cd build
 # Generate the Makefile using CMake
 cmake .. -DCMAKE_BUILD_TYPE=Release
 # Build the library
 make
 # Install the library
 sudo make install
+
+cd ../..

eaafd5c6cd0b9216e5051f38b2e7851961f2d3a5
yishen <yishen@localhost>
2023-03-31 20:10:42 -0600

    update scripts

--- install_grpc.sh
+++ install_grpc.sh
@@ -3,10 +3,7 @@ export MY_INSTALL_DIR=$HOME/.local
 mkdir -p $MY_INSTALL_DIR
 export PATH="$MY_INSTALL_DIR/bin:$PATH"
 
-sudo apt-get remove cmake
-wget -q -O cmake-linux.sh https://github.com/Kitware/CMake/releases/download/v3.19.6/cmake-3.19.6-Linux-x86_64.sh
-sh cmake-linux.sh -- --skip-license --prefix=$MY_INSTALL_DIR
-rm cmake-linux.sh
+sudo apt install -y cmake
 
 sudo apt update -y
 sudo apt install -y build-essential autoconf libtool pkg-config libsystemd-dev 
